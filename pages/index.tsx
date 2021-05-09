4402b377f4afd2350ad6190233189389650dd68d
victorpremoli <victor.premoli57@gmail.com>
2021-05-09 12:19:43 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import { GetServerSideProps } from 'next';
+import { FormEvent, useContext, useState } from 'react'
+import { parseCookies } from 'nookies'
+
+import { AuthContext } from '../context/AuthContext';
+import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { singIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    await singIn(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">ENTRAR</button>
+    </form>
+  )
+}
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+});
